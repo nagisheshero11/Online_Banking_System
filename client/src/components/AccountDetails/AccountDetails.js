809fd583bdc53af316c1c62276d2a9f184421bdc
nagisheshero11 <hemanthsai964@gmail.com>
2025-10-07 23:44:16 +0530

    added account deatils and paybill folders

---
+++ client/src/components/AccountDetails/AccountDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AccountDetails = () => {
+  return (
+    <div className="page-content">
+      <h1>Account Details Page</h1>
+      <p>This page will display the detailed account information.</p>
+    </div>
+  );
+};
+
+export default AccountDetails;
